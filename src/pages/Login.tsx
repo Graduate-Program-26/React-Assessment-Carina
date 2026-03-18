62dd3695b27b1c9555f6cd49b73f063c81773fbc
Carina Diesel <cdiesel@dvtsoftware.com>
2026-03-18 14:59:53 +0200

    feat: Added clerk provider for auth handling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,12 @@
 import { LoginForm } from "@/components/Shadcn/login-form"
+import { SignIn } from "@clerk/clerk-react"
 
-export default function Home() {
+export default function Login() {
   // Page to be used as search all github users and preview
   return (
     <div className="flex h-[80vh] items-center justify-center">
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <SignIn routing="path" path="/login" />
     </div>
   )
 }
