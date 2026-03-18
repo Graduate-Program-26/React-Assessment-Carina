21567bd39e77284cdbea315fe2025fabef028199
Carina Diesel <cdiesel@dvtsoftware.com>
2026-03-18 13:38:05 +0200

    chore: Imported all fields for Shadcn login component block

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,10 @@
+import { LoginForm } from "@/components/Shadcn/login-form"
+
 export default function Home() {
   // Page to be used as search all github users and preview
-  return <div>Hello</div>
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  )
 }
